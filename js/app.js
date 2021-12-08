8f2dd5a8f82690d76b82d5951ad6f644f9ccc8a3
Kevin van Nieukerke <k.v.nieukerke@gmail.com>
2021-12-08 16:18:52 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,73 @@
+const API_KEY = "AIzaSyAUCSTzBPyrChojzUP8KzW28_VIy4wO058";
+const PLAYLIST_ID = "PLZ4ny09ZUze2Risqpzt9rvV3yLzc_RPGi";
+let list = [];
+
+function getVideos(t) {
+	var url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${API_KEY}&playlistId=${PLAYLIST_ID}&maxResults=50`;
+	if (t != undefined) {
+		url = url + "&pageToken=" + t;
+	}
+	$.ajax({
+		type: "GET",
+		url: url,
+		dataType: "json",
+		success: function (html) {
+			// add html.items to list array
+			list = list.concat(html.items);
+
+			if (html.nextPageToken != undefined) {
+				getVideos(html.nextPageToken);
+			} else {
+				displayVideos(list);
+			}
+		},
+	});
+}
+
+function displayVideos(videos) {
+	var videoContainer = document.querySelector("#video-container");
+	videoContainer.innerHTML = "";
+	for (var i = 0; i < videos.length; i++) {
+		var video = videos[i];
+		var videoLink = document.createElement("a");
+		videoLink.className = "video";
+		videoLink.setAttribute(
+			"href",
+			"https://www.youtube.com/watch?v=" +
+				video.snippet.resourceId.videoId
+		);
+		var thumbnail = document.createElement("img");
+		if (video.snippet.thumbnails.medium != undefined) {
+			thumbnail.setAttribute("src", video.snippet.thumbnails.medium.url);
+		} else {
+			continue;
+		}
+		videoLink.appendChild(thumbnail);
+
+		var info = document.createElement("div");
+		info.classList.add("video-info");
+
+		var title = document.createElement("h3");
+		title.innerHTML = video.snippet.title;
+
+		var description = document.createElement("p");
+		description.innerHTML = video.snippet.description;
+
+		info.appendChild(title);
+		info.appendChild(description);
+
+		videoLink.appendChild(info);
+
+		videoContainer.appendChild(videoLink);
+	}
+}
+
+getVideos();
+
+const random = document.querySelector("#random");
+random.addEventListener("click", function () {
+	var randomVideo = list[Math.floor(Math.random() * list.length)];
+	window.location.href =
+		"https://www.youtube.com/watch?v=" +
+		randomVideo.snippet.resourceId.videoId;
+});
